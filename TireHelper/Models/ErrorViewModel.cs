9b029f89023a59d8d5bdd3d5eb46e9aff9c4f42c
Tim-coder-ops <timweigel63@gmail.com>
2020-09-07 20:43:29 -0700

    Initial commit

---
+++ TireHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TireHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
